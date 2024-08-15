9c0ec29d9e1f931c6a3a1f16546ed2ce8e654f73
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-08-15 11:29:00 +0530

    Adding Color Pallete

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Navbar/Layout";
+import PostPage from "./PostPage/PostPage";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            path="/home"
+            element={
+              <div className="Test">
+                <h1>xHome</h1>
+              </div>
+            }
+          />
+          <Route path="/postJob" element={<PostPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/PostPage/PostPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PostPage() {
+  return (
+    <div>
+      <h1>POST PAGE</h1>
+    </div>
+  );
+}
