5c5ed775d1d3277fb346849dfa139b04263bbde9
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-08-19 13:02:57 +0530

    Bug Fix

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ dotenv.config();
 
 const app = express();
 
+let MongoClient = mongodb.MongoClient;
 const client = new MongoClient(process.env.MONGO_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
