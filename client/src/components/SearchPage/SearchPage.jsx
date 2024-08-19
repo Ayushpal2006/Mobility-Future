510dabc04c7a382134d55e0647958e7d0fb9fdfa
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-08-19 10:43:28 +0530

    initial style setup

--- client/src/components/SearchPage/SearchPage.jsx
+++ client/src/components/SearchPage/SearchPage.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SearchPage.module.css";
 import SelectDD from "./components/SelectDD";
 import data from "../filter.json";
 import PostCard from "./components/PostCard";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function SearchPage() {
   function handleChange(id) {
     console.log(id);
   }
+  const [postsData, setPostsData] = useState([]);
 
-  console.log(data);
+  useEffect(() => {
+    const getData = async () => {
+      const result = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}api/posts`
+      );
+      setPostsData(result.data);
+    };
+    getData();
+  }, []);
+
+  console.log(postsData);
 
   return (
     <div className={styles.container}>
@@ -48,10 +61,9 @@ export default function SearchPage() {
         <h1 className="amsterdam">Search Page</h1>
         <hr />
         <div className={styles.cardsContainer}>
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
+          {postsData.map((obj) => {
+            return <PostCard data={obj} key={obj.id} />;
+          })}
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ import cors from "cors";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import { default as mongodb, ObjectId, ServerApiVersion } from "mongodb";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import connectDB from "./db/mongoose-connection.js";
 import authRouter from "./routes/auth.js";
-import postRouter from './routes/postJob.js'
+import postRouter from "./routes/postJob.js";
 
 // Load environment variables
 dotenv.config();
@@ -16,6 +16,18 @@ let MongoClient = mongodb.MongoClient;
 
 const app = express();
 
+const client = new MongoClient(process.env.MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//Define Collections here
+const db = client.db("test");
+const usersCollection = db.collection("users");
+const postsCollection = db.collection("postjobs");
 
 // Middleware
 app.use(express.json());
@@ -34,19 +46,17 @@ app.use(cors());
 app.use("/api/auth", authRouter);
 app.use("/api/postJob", postRouter);
 
-// const client = new MongoClient(process.env.MONGO_URI, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
+app.get("/api/posts", async (req, res) => {
+  const result = await postsCollection.find({ driver: "NA" }).toArray();
+  res.send(result);
+});
+
+// app.post('/assignDriver', (req,res)=>{
+//   const driver_id = req.body.driver_id;
+//   const
+
+//   res.sendStatus(200);
 // });
-
-// //Define Collections here
-// const db = client.db("MobilityFuture");
-// const usersCollection = db.collection("users");
-// const postsCollection = db.collection("posts");
-
 // Starting the server
 const PORT = process.env.PORT || 4000;
 
