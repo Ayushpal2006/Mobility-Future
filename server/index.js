0c837bbea4cc2a22ba5b9be6be2a38f86034553d
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-08-28 20:43:49 +0530

    FIxing CORS Error

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 // app.use(cors());
 
